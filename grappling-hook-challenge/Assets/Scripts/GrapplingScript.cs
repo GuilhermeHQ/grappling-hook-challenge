0e1c4919f905df9797b77a0706a948e998a38a21
guilherme_hq <quintana.guilherme@gmail.com>
2021-11-27 20:50:22 -0300

    add grappling gun logic to game

--- grappling-hook-challenge/Assets/Scripts/GrapplingScript.cs
+++ grappling-hook-challenge/Assets/Scripts/GrapplingScript.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class GrapplingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappeable;
+    public Transform gunTip, camera, player;
+    [SerializeField] private float maxDistance = 100f;
+    [SerializeField] private float spring = 4.5f;
+    [SerializeField] private float damper = 7f;
+    [SerializeField] private float massScale = 4.5f;
+    private SpringJoint joint;
+
+
+    void Awake()
     {
-        
+        lr = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    //Call whenever we want to start a grapple
+    void StartGrapple()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(origin: camera.position, direction: camera.forward, out hit, maxDistance, whatIsGrappeable))
+
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(a: player.position, b: grapplePoint);
+
+            //The distance grapple will try to keep from grapple point
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+
+            //Change these values to balance grapple
+            joint.spring = spring;
+            joint.damper = damper;
+            joint.massScale = massScale;
+
+        }
+    }
+
+    void DrawRope()
+    {
+        lr.SetPosition(index: 0, gunTip.position);
+        lr.SetPosition(index: 1, grapplePoint);
+    }
+    void StopGrapple()
+    {
+
     }
 }
+
+  
